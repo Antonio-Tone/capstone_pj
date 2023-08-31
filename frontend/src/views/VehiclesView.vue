b98d8c8d4cdc524e7ba6635c190161fc4942d5fa
Antonio-Tone <antoniotone815@gmail.com>
2023-08-31 16:01:09 +0200

    frontend

---
+++ frontend/src/views/VehiclesView.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="prods">
+    <!-- <div class="row mb-2">
+    <form class="d-flex mb-5 mt-4 search" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn search" type="submit">Search</button>
+      </form>
+    </div> -->
+    <div class="container">
+      <h2 class="class-display2 fs-1 mb-5">showroom</h2>
+      <div
+        class="row justify-content-center gap-6"
+        v-if="vehicles"
+      >
+        <div class="col-4" v-for="car in vehicles" :key="car.vehicleID">
+          <div
+            class="card"
+            style="width: 18rem"
+            @mouseover="hoveredVehicle = car"
+            @mouseleave="hoveredVehicle = null"
+          >
+            <img
+              :src="hoveredVehicle === car ? car.hoveredURL : car.imageURL"
+              class="card-img-top"
+              :alt="car.vehicleName"
+            />
+            <div class="card-body">
+              <h5 class="card-title">{{ car.vehicleName }} <br /></h5>
+              <center>
+                <button class="view" @click="viewVehicle(car.vehicleID)">
+                  <span>View More</span>
+                  <svg
+                    class="arrow"
+                    viewBox="0 0 448 512"
+                    height="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+                    ></path>
+                  </svg>
+                </button>
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else class="row">
+        <Spinner/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import Spinner from "../components/SpinnerComp.vue";
+export default {
+  components: {
+    Spinner,
+  },
+  computed: {
+    vehicles() {
+      return this.$store.state.vehicles;
+    },
+  },
+  methods: {
+    viewVehicle(vehicleID) {
+      const chosenVehicle = this.vehicles.find(
+        (vehicle) => vehicle.vehicleID === vehicleID
+      );
+      this.$store.commit("setSingleVehicle", chosenVehicle);
+      this.$router.push({ name: "Vehicle", params: { vehicleID: vehicleID } });
+    },
+  },
+  mounted() {
+    this.$store.dispatch("fetchVehicles");
+  },
+  data() {
+    return {
+      hoveredVehicle: null,
+    };
+  },
+};
+</script>
